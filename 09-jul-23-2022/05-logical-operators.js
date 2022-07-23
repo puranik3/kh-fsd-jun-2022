00417638a9a4ce09be4e1219b560c29de2622c8f
Prashanth Puranik <puranik@digdeeper.in>
2022-07-23 21:41:18 +0530

    || operator

--- 09-jul-23-2022/05-logical-operators.js
+++ 09-jul-23-2022/05-logical-operators.js
@@ -1,12 +1,32 @@
 // ! (not), && (and), || (or)
 let bankBalance = 5000, invoiceAmount = 2000, maxTransaction = 2500;
 
+// && operator
 // true && false -> false (&& ensures all conditions are met)
 let proceed = bankBalance >= invoiceAmount && invoiceAmount <= maxTransaction;
 
 if( proceed ) {
     bankBalance = bankBalance - invoiceAmount;
     console.log( 'Payment successful. New balance = ' + bankBalance );
+} else {
+    console.log( 'Payment declined' );
+}
+
+// || operator
+// true || false -> true
+// false || true -> true
+// true || true -> true
+// false || false -> false
+invoiceAmount = 7000;
+let ccLimit = 10000;
+
+// invoiceAmount <= bankBalance -> false (7000 <= 2000 -> false)
+// invoiceAmount <= ccLimit -> true (7000 <= 10000 -> true)
+// false || true -> true
+proceed = invoiceAmount <= bankBalance || invoiceAmount <= ccLimit;
+
+if( proceed ) {
+    console.log( 'Payment successful' );
 } else {
     console.log( 'Payment declined' );
 }
