7763a5c36b6560c84e73618505d9af11d92bddfa
Prashanth Puranik <puranik@digdeeper.in>
2022-07-23 20:06:56 +0530

    string ops

---
+++ 09-jul-23-2022/01-const.js
@@ -0,0 +1,5 @@
+// const age; // error
+// we declre a variable using "const" if we intend not to change its value. if we do try to do so, an error is thrown
+const greeting = 'Good afternoon';
+// greeting = 'Good morning'; // error
+
---
+++ 09-jul-23-2022/02-string.js
@@ -0,0 +1,5 @@
+const quote = 'With great power comes great responsibility';
+console.log( quote.length );
+console.log( quote[0] );
+console.log( quote[42] );
+console.log( quote[quote.length - 1] );
