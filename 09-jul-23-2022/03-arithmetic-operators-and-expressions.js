c22d9e802136f0a1e5338924057fd233b9cbd4a1
Prashanth Puranik <puranik@digdeeper.in>
2022-07-23 20:22:06 +0530

    arithmetic operators and expressions

---
+++ 09-jul-23-2022/03-arithmetic-operators-and-expressions.js
@@ -0,0 +1,24 @@
+// arithmetic operators
+// +, -, *, /, %, **
+// 2 + 3 -> expression -> expression has a value
+let result;
+result = 2 + 3; // statement (a line of code)
+
+console.log( result );
+
+let x = 12, y = 13;
+console.log( x + y );
+console.log( x + 14 );
+console.log( x - y );
+console.log( x * y );
+console.log( x / y );
+console.log( 20 % y ); // 7
+console.log( 3 ** 2 ); // 9
+
+// e = mc^2
+const m = 2, c = 2.99e8;
+const e = m * c ** 2;
+console.log( e );
+
+// Declare variables, calculate interest, and print out the result
+// interest = Principal * ratePerYear * numYears / 100;
