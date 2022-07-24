087c4a21fbc2ff40c59812954dd3dafc33aa6e15
Prashanth Puranik <puranik@digdeeper.in>
2022-07-24 21:32:15 +0530

    adding and editing properties

--- 10-jul-24-2022/07-object.js
+++ 10-jul-24-2022/07-object.js
@@ -3,6 +3,7 @@
 
 // An object is a collection of key-value pairs
 // keys are strings (understood - no need to quote it)
+// keys are also called "properties"
 // values can be of any data type
 // key1: value1, key2: value2, ...
 const john = {
@@ -29,3 +30,15 @@ console.log( john.name );
 console.log( john.dept );
 console.log( john.emailAddresses[1] ); // second email
 console.log( john.address.city ); // john's city
+
+// adding / editing properties is done the exact same way
+john.name = 'Jonathan';
+john.emailAddresses.push( 'john@yahoo.com' );
+john.address.line = '#64 Rosewell';
+john.favoriteColor = 'Blue';
+john.spouse = {
+    name: 'Jane',
+    age: 28
+};
+
+console.log( john );
