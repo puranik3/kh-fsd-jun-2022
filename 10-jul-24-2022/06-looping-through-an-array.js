9204b8bc935d51d48f4bccd01ac21041b03ba88f
Prashanth Puranik <puranik@digdeeper.in>
2022-07-24 21:05:23 +0530

    small exercise

--- 10-jul-24-2022/06-looping-through-an-array.js
+++ 10-jul-24-2022/06-looping-through-an-array.js
@@ -2,4 +2,5 @@ const cities = [ 'New York', 'London', 'Paris' ];
 
 for( let idx = 0; idx < cities.length; ++idx ) {
     console.log( cities[idx] );
+    // EXERCISE: Print the number of characters in the city as well
 }
