0973b6bec429f80cee6d54fc85cdd10b962e4599
Prashanth Puranik <puranik@digdeeper.in>
2022-07-24 22:54:33 +0530

    exercise on functions

---
+++ 10-jul-24-2022/13-simple-interest.js
@@ -0,0 +1,7 @@
+/**
+ * EXERCISE:
+ * 
+ * calculateInterest1() in function expression syntax. Call it a couple of times.
+ * 
+ * calculateInterest2() in arrow function syntax. Call it a couple of times.
+ */
