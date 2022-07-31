c0db5027a14412046b02ed9b4e24eb77fc4f7bd0
Prashanth Puranik <puranik@digdeeper.in>
2022-07-31 20:47:20 +0530

    RegExp

--- 12-jul-31-2022/02-Object.js
+++ 12-jul-31-2022/02-Object.js
@@ -14,12 +14,17 @@ console.log( keys );
 // let us find the value for each key...
 // keys = [ 'name', 'age' ]
 keys.forEach(
-    function( key ) {
+    key => {
         console.log( key, john[key] ); // key and its value
 
         // if the value happens to be an array, let us print its first item...
+        // Array is a class in JS (all arrays are objects of this class)
         if( john[key] instanceof Array ) {
             console.log( `first item of ${key}: ${john[key][0]}` );
         }
     }
 );
+
+const today = new Date();
+console.log( today instanceof Date ); // true
+console.log( keys instanceof Date ); // false
---
+++ 12-jul-31-2022/03-RegExp.js
@@ -0,0 +1,19 @@
+// const phonePattern = new RegExp( '\\d' )
+// const phonePattern = /[0-9]/;
+const phonePattern1 = /\d{3}-\d{7}/;
+const phonePattern2 = /^\d{3}-\d{7}$/;
+
+console.log( phonePattern1.test( '123-4567890' ) ); // true
+console.log( phonePattern1.test( '123-45678901' ) ); // true! (the entire string is not matched)
+
+console.log( phonePattern2.test( '123-4567890' ) ); // true
+console.log( phonePattern2.test( '123-45678901' ) ); // false
+
+console.clear();
+
+const emailPattern = /^[A-Za-z0-9_\.]+@(example|gmail|yahoo)\.[A-Za-z]{2,3}$/;
+
+console.log( emailPattern.test( 'john.doe@example.com' ) ); // true
+console.log( emailPattern.test( 'john.doe2@example.com' ) ); // true
+console.log( emailPattern.test( 'john.doe@outlook.com' ) ); // false (outlook not allowed)
+console.log( emailPattern.test( 'john%doe@gmail.com' ) ); // false (% not allowed)
