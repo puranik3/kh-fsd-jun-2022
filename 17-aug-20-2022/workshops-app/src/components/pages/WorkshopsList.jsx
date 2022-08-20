85e9836dea412c13096b5fefc0da8380dc94b0f4
Prashanth Puranik <puranik@digdeeper.in>
2022-08-20 21:16:23 +0530

    Fetching data on load

--- 17-aug-20-2022/workshops-app/src/components/pages/WorkshopsList.jsx
+++ 17-aug-20-2022/workshops-app/src/components/pages/WorkshopsList.jsx
@@ -7,9 +7,31 @@ class WorkshopsList extends Component {
             <div>
                 <h1>List of Workshops</h1>
                 <hr />
-
+                <div>Fetching list of workshops. Please wait...</div>
             </div>
         );
+    }
+
+    // side-effect
+    // useEffect() is the equivalent in function component
+    // executes as soon as the component shows up
+    componentDidMount() {
+        fetch( `http://workshops-server.herokuapp.com/workshops` )
+            .then(
+                function( response ) {
+                    return response.json();
+                }
+            )
+            .then(
+                function( workshops ) {
+                    console.log( workshops );
+                }
+            )
+            .catch(
+                function( error ) {
+                    alert( error.message );
+                }
+            )
     }
 };
 
