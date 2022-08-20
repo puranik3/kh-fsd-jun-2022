8cbd5494e22884b870979ab0fd9b97d92dd16c3b
Prashanth Puranik <puranik@digdeeper.in>
2022-08-20 20:59:14 +0530

    Creating a class-based component

--- 17-aug-20-2022/workshops-app/src/components/pages/WorkshopsList.jsx
+++ 17-aug-20-2022/workshops-app/src/components/pages/WorkshopsList.jsx
@@ -1,11 +1,16 @@
-const WorkshopsList = () => {
-    return (
-        <div>
-            <h1>List of Workshops</h1>
-            <hr />
+import { Component } from 'react';
 
-        </div>
-    );
+/** Component using class */
+class WorkshopsList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>List of Workshops</h1>
+                <hr />
+
+            </div>
+        );
+    }
 };
 
 export default WorkshopsList;
