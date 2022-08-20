e96fa7fb3fb95a42a40e23fed3c7b290a422f0c7
Prashanth Puranik <puranik@digdeeper.in>
2022-08-20 20:42:27 +0530

    List of workshops

---
+++ 17-aug-20-2022/workshops-app/src/components/App.jsx
@@ -0,0 +1,9 @@
+import WorkshopsList from "./pages/WorkshopsList";
+
+const App = () => {
+    return (
+        <WorkshopsList />
+    );
+};
+
+export default App;
---
+++ 17-aug-20-2022/workshops-app/src/components/pages/WorkshopsList.jsx
@@ -0,0 +1,11 @@
+const WorkshopsList = () => {
+    return (
+        <div>
+            <h1>List of Workshops</h1>
+            <hr />
+
+        </div>
+    );
+};
+
+export default WorkshopsList;
