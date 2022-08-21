53e97a4151101deda982534b7d605e33a51bdf88
Prashanth Puranik <puranik@digdeeper.in>
2022-08-21 21:03:59 +0530

    fetching and showing data

--- 18-aug-21-2022/workshops-app/src/components/pages/WorkshopsList.jsx
+++ 18-aug-21-2022/workshops-app/src/components/pages/WorkshopsList.jsx
@@ -1,5 +1,6 @@
 // "hooks" - functions that can be used ONLY in function components (each hooks adds some functionality to function components that were earlier available ONLY in class components)
 import { useState, useEffect } from 'react';
+import axios from "axios";
 
 const WorkshopsList = ( { sm, lg } ) => {
     // an array with 2 items
@@ -16,7 +17,23 @@ const WorkshopsList = ( { sm, lg } ) => {
     // if the dependency array is an empty array, the side-effect executes only after the first render (similar to using componentDidMount in a class component)
     useEffect(
         () => {
-            console.log( 'first render' );
+            console.log( 'after first render' );
+
+            axios
+                .get(`http://workshops-server.herokuapp.com/workshops`)
+                .then(
+                    (response) => {
+                        const workshops = response.data;
+
+                        // update the state using the appropriate setters
+                        setLoading( false );
+                        setWorkshops( workshops );
+                    }
+                )
+                .catch((error) => {
+                    setLoading( false );
+                    setError( error );
+                });
         },
         [] // dependency array
     );
@@ -28,8 +45,36 @@ const WorkshopsList = ( { sm, lg } ) => {
             <h1 className="my-3">List of Workshops</h1>
             <hr />
             {
-                <div>Fetching list of workshops. Please wait...</div>
+                loading === true && (
+                    <div>Fetching list of workshops. Please wait...</div>
+                )
             }
+            {loading === false && error === null && (
+                <div className="row">
+                    {
+                        workshops.map((workshop) => (
+                            <div className={`col-12 col-sm-${sm} col-lg-${lg} d-flex mb-3`}>
+                                <div className="card p-4 w-100">
+                                    <img
+                                        src={workshop.imageUrl}
+                                        className="card-img-top"
+                                        alt={workshop.name}
+                                    />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{workshop.name}</h5>
+                                        <a href="/" className="btn btn-primary">
+                                            Know more
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            )}
+            {loading === false && error !== null && (
+                <div>{error.message}</div>
+            )}
         </div>
     );
 };
