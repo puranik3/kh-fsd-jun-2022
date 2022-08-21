7d7e398290a492585d21eba040bcb63ea412a71b
Prashanth Puranik <puranik@digdeeper.in>
2022-08-21 19:57:51 +0530

    using props in class components

---
+++ 18-aug-21-2022/workshops-app/src/components/App.jsx
@@ -0,0 +1,11 @@
+import WorkshopsList from "./pages/WorkshopsList";
+
+const App = () => {
+    return (
+        <div className="container">
+            <WorkshopsList sm={6} lg={3} />
+        </div>
+    );
+};
+
+export default App;
